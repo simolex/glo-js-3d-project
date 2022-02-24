510c62ce380307389a92fddf1d423c3d65f729d1
simolex <simolex@yandex.ru>
2022-02-24 13:49:02 +0300

    Проверка на валидность текстовых полей

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import moduleMenu from "./modules/menu";
 import moduleModal from "./modules/modal";
 import serviceBlock from "./modules/serviceBlock";
 import moduleCalculate from "./modules/calculate";
+import validateForm1 from "./modules/validateForm1";
+import validateForm2 from "./modules/validateForm2";
+import validateForm3 from "./modules/validateForm3";
+
+//validateForm
 
 //import smoothScroll from "./modules/smoothScroll";
 
@@ -11,3 +16,6 @@ moduleMenu();
 moduleModal();
 serviceBlock();
 moduleCalculate();
+validateForm1();
+validateForm2();
+validateForm3();
