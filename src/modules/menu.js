fc52b8179c9b1058fd12b05ad0d90bab0ea34fba
simolex <simolex@yandex.ru>
2022-02-28 15:30:01 +0300

    Перенос двух слушателей меню на документ

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,13 +1,14 @@
 import smoothScroll from "./smoothScroll";
 export const menu = () => {
-  const menuButton = document.querySelector(".menu");
+  // const menuButton = document.querySelector(".menu");
   const menuContent = document.querySelector("menu");
 
   const toggleMenu = (e) => {
-    if (e.target.matches("ul>li>a")) {
+    if (e.target.matches("menu ul>li>a")) {
       e.preventDefault();
       smoothScroll(e.target);
     } else if (e.target.classList.contains("close-btn")) {
+      e.preventDefault();
     } else if (e.target.closest(".menu")) {
     } else {
       return;
@@ -15,6 +16,7 @@ export const menu = () => {
     menuContent.classList.toggle("active-menu");
   };
 
-  menuButton.addEventListener("click", (e) => toggleMenu(e));
-  menuContent.addEventListener("click", (e) => toggleMenu(e));
+  document.addEventListener("click", (e) => toggleMenu(e));
+  // menuButton.addEventListener("click", (e) => toggleMenu(e));
+  // menuContent.addEventListener("click", (e) => toggleMenu(e));
 };
