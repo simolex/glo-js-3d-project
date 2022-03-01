238a4b993926fdef5eaec29a86bfe9c82635bb70
simolex <simolex@yandex.ru>
2022-03-01 08:46:28 +0300

    поправлена логика обработки меню

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,22 +1,21 @@
 import smoothScroll from "./smoothScroll";
 export const menu = () => {
-  // const menuButton = document.querySelector(".menu");
   const menuContent = document.querySelector("menu");
 
   const toggleMenu = (e) => {
-    if (e.target.matches("menu ul>li>a")) {
+    if (e.target.closest(".menu") || e.target.closest("menu")) {
       e.preventDefault();
-      smoothScroll(e.target);
-    } else if (e.target.classList.contains("close-btn")) {
-      e.preventDefault();
-    } else if (e.target.closest(".menu")) {
-    } else {
+      if (e.target.matches("ul>li>a")) {
+        smoothScroll(e.target);
+      } else if (e.target.classList.contains("close-btn")) {
+      } else if (e.target.closest("menu")) {
+        return;
+      }
+    } else if (!menuContent.classList.contains("active-menu")) {
       return;
     }
     menuContent.classList.toggle("active-menu");
   };
 
   document.addEventListener("click", (e) => toggleMenu(e));
-  // menuButton.addEventListener("click", (e) => toggleMenu(e));
-  // menuContent.addEventListener("click", (e) => toggleMenu(e));
 };
